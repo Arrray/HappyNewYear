0658622d7e6d06f97f7d18a344e7976e1f3e1cbb
Arrray <gjc@hrsoft.net>
2016-02-23 16:19:30 +0800

    page切换完成

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
  * Created by Array on 16/2/23.
  */
 window.onload = function(){
+    //获取元素
+    var page1 = doctment.getElementById("page1");
+    var page2 = doctment.getElementById("page2");
+    var page3 = doctment.getElementById("page3");
+
     var music = document.getElementById("music");
     var audio = document.getElementsByTagName("audio")[0];
 
@@ -40,4 +45,17 @@ window.onload = function(){
                     //this.style.webkitAnimationPlayState = "paused";
                 }
     }, false);
+
+    //点击屏幕,开启好运2016
+    page1.addEventListener("touchstart", function(event){
+        page1.style.display = "none";
+        page2.style.display = "block";
+        page3.style.display = "block";
+        page3style.top      = "100%";
+
+        setTimeout(function(){
+            page2.setAttribute("class","page fadeOut");
+            page3.setAttribute("class","page fadeIn");
+        },5500);
+    },false);
 };
