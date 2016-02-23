e1f78924a6eae72b174466a03c6896918d41ecac
Arrray <gjc@hrsoft.net>
2016-02-23 20:54:16 +0800

    项目全部完成

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
  */
 window.onload = function(){
     //获取元素
-    var page1 = doctment.getElementById("page1");
-    var page2 = doctment.getElementById("page2");
-    var page3 = doctment.getElementById("page3");
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
 
     var music = document.getElementById("music");
     var audio = document.getElementsByTagName("audio")[0];
@@ -51,7 +51,7 @@ window.onload = function(){
         page1.style.display = "none";
         page2.style.display = "block";
         page3.style.display = "block";
-        page3style.top      = "100%";
+        page3.style.top     = "100%";
 
         setTimeout(function(){
             page2.setAttribute("class","page fadeOut");
